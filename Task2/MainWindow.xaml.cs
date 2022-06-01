412982172a60872c80d12bdecdd0bf91eab526b2
Andrew <1@1.rugit config --global user.name Andrewcd F:\ITVDN\2 git config --global user.email 1@1.ru>
2022-06-01 20:02:33 +0300

    Add project files.

---
+++ Task2/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace Task2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource _tokenSource = new CancellationTokenSource();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonConnect_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                await ConnectAsync();
+            }
+            catch (Exception exception)
+            {
+                TextBox.Dispatcher.Invoke(() => TextBox.Text = exception.Message);
+                _tokenSource = new CancellationTokenSource();
+            }
+
+        }
+
+        private void ButtonDisconnect_Click(object sender, RoutedEventArgs e)
+        {
+            _tokenSource.Cancel();
+        }
+
+        private async Task ConnectAsync()
+        {
+            try
+            {
+                await Task.Run(Connect, _tokenSource.Token);
+                await GetDataAsync();
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+
+        private bool Connect()
+        {
+            for (int i = 0; i < 5; i++)
+                try
+                {
+                    _tokenSource.Token.ThrowIfCancellationRequested();
+                    TextBox.Dispatcher.Invoke(() => TextBox.Text = $"Соединение... {i}");
+                    Thread.Sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    throw;
+                }
+
+            TextBox.Dispatcher.Invoke(() => TextBox.Text = "Подключен к базе данных");
+            return true;
+        }
+
+        private async Task GetDataAsync()
+        {
+            Task task = new Task(() =>
+            {
+                int i = 1;
+                try
+                {
+                    while (true)
+                    {
+                        _tokenSource.Token.ThrowIfCancellationRequested();
+                        Thread.Sleep(100);
+                        TextBoxData.Dispatcher.Invoke(() => TextBoxData.Text = "Данные получены " + i);
+                        i++;
+                    }
+                }
+                catch (Exception e)
+                {
+                    TextBoxData.Dispatcher.Invoke(() => TextBoxData.Text = e.Message);
+                    TextBox.Dispatcher.Invoke(() => TextBox.Text = "Соединнение разорвано");
+                    _tokenSource = new CancellationTokenSource();
+                }
+            });
+
+            task.Start();
+            await task;
+        }
+    }
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    internal class Program
+    {
+        private static int _counter = 0;
+
+        private static readonly object _block = new object();
+
+        private static void Function()
+        {
+            lock (_block)
+            {
+                for (int i = 0; i < 10; ++i)
+                {
+                    Thread.Sleep(20);
+                    Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} - {++_counter}");
+                }
+            }
+        }
+
+        private static async Task FunctionAsync()
+        {
+            await Task.Run(Function);
+        }
+
+        static void Main()
+        {
+            Task[] tasks = { FunctionAsync(), FunctionAsync(), FunctionAsync() };
+            Task.WaitAll(tasks);
+            Console.ReadKey();
+        }
+    }
+
+}
